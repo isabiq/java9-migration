18c58fb032f51190b687b0cf1360167b6e5aef47
isabiq <sabiq.ihab@hotmail.fr>
2017-12-16 17:50:01 -0500

    add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.iluwatar.cqrs {
+    requires hibernate.jpa;
+    requires slf4j.api;
+    requires hibernate.core;
+    requires java.naming;
+
+    exports com.iluwatar.cqrs.commandes;
+    exports com.iluwatar.cqrs.dto;
+    exports com.iluwatar.cqrs.queries;
+
+    opens com.iluwatar.cqrs.domain.model;
+    opens com.iluwatar.cqrs.dto;
+
+}
